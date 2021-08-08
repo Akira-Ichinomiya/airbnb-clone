e30072b5ab06595a2f632bb816b727bdb11db84b
Akira_Ichinomiya <tkmt123@naver.com>
2021-08-08 23:13:46 +0900

    쿼리셋 이해, 모델 수정

--- rooms/admin.py
+++ rooms/admin.py
@@ -8,6 +8,11 @@ class ItemAdmin(admin.ModelAdmin):
 
     """Item Admin Definition"""
 
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
     pass
 
 
@@ -57,6 +62,7 @@ class RoomAdmin(admin.ModelAdmin):
         "check_out",
         "instant_book",
         "count_amenities",
+        "count_photos",
     )
 
     ordering = ("name", "price", "bedrooms")
@@ -79,7 +85,8 @@ class RoomAdmin(admin.ModelAdmin):
     def count_amenities(self, obj):
         return obj.amenities.count()
 
-    count_amenities.short_description = "hi!"
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
