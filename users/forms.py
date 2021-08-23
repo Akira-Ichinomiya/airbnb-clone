8a7b9d94bc3140490818d69d6943dbeb85236fa9
Akira_Ichinomiya <tkmt123@naver.com>
2021-08-23 18:11:19 +0900

    Sign Up 구현 완료

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 from . import models
 
 
@@ -18,3 +19,42 @@ class LoginForm(forms.Form):
                 self.add_error("password", forms.ValidationError("Password is wrong."))
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User does not exist."))
+
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = models.User
+        fields = ["first_name", "last_name", "email"]  # clean검사를 알아서 해줌
+
+    # first_name = forms.CharField(max_length=50)
+    # last_name = forms.CharField(max_length=50)
+    # email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(
+        widget=forms.PasswordInput, label="Confirm Password"
+    )
+
+    # def clean_email(self):
+    #     email = self.cleaned_data.get("email")
+    #     try:
+    #         models.User.objects.get(email=email)
+    #         raise forms.ValidationError("Email already exists.")
+    #     except models.User.DoesNotExist:
+    #         return email
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get("password")
+        password_confirm = self.cleaned_data.get("password_confirm")
+        print(password)
+        print(password_confirm)
+
+        if password_confirm != password:
+            raise forms.ValidationError("Passwords do not match.")
+        else:
+            return password
+
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
+        user.username = self.cleaned_data.get("email")
+        user.set_password(self.cleaned_data.get("password"))
+        user.save()
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render, reverse
+from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import FormView
-from django.urls import reverse_lazy
-from django.shortcuts import render, redirect, reverse
-from django.contrib.auth import authenticate, login, logout
+
 from . import forms
 
 
@@ -24,3 +26,24 @@ class LoginView(FormView):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignupView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignupForm
+    success_url = reverse_lazy("core:home")
+
+    initial = {
+        "first_name": "Lee",
+        "last_name": "Heesu",
+        "email": "heesu0730@nate.com",
+    }
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
