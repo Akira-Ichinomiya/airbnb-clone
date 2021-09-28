985e4f4178a6f290b16b22dbd8732f36728d4f08
lhs <heesulee@lhsui-MacBookPro.local>
2021-09-28 22:05:31 +0900

    예약 시 사이날짜들 입력불가하도록 설정완료

--- reservations/admin.py
+++ reservations/admin.py
@@ -18,3 +18,14 @@ class ReservationAdmin(admin.ModelAdmin):
     )
 
     list_filter = ("status",)
+
+
+@admin.register(models.BookedDay)
+class BookedDayAdmin(admin.ModelAdmin):
+
+    """Booked Day Admin Definition"""
+
+    list_display = (
+        "day",
+        "reservation",
+    )
